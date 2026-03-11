a9da0acfa6f946432f69fd81f5c9c308c1f80234
Ldavid1995 <laverruz40728@ufide.ac.cr>
2026-03-10 21:25:45 -0600

    Primer commit
    Primer codigo

---
+++ src/main/java/com/concentrados/Danny/DannyApplication.java
@@ -0,0 +1,13 @@
+package com.concentrados.Danny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DannyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DannyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concentrados/Danny/DannyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concentrados.Danny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DannyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
