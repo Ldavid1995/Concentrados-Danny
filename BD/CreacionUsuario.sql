7d4d1b0366ae8f5e94ba533c8c7ebde32065ce2b
nay-araya12 <nayarayat@gmail.com>
2026-07-16 02:54:36 -0600

    commit nay: se modificaron las tablas

--- BD/CreacionUsuario.sql
+++
@@ -1,9 +0,0 @@
-
--- 2. Crear el usuario nuevamente
-CREATE USER Concentrados IDENTIFIED BY "Proyecto$2026";
-
--- 3. Asignarle espacio de almacenamiento ilimitado
-ALTER USER Concentrados QUOTA UNLIMITED ON DATA;
-
--- 4. Otorgarle los privilegios de desarrollo necesarios
-GRANT CREATE SESSION, CREATE TABLE, CREATE VIEW, CREATE SEQUENCE, CREATE PROCEDURE, CREATE TRIGGER TO Concentrados;
